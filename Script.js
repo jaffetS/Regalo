c7bb1478577a18ded8dbfe9ce71eae7f98d8209f
Jaffet <jaffetsereno@gmail.com>
2025-03-14 16:23:38 -0600

    Se agrega el Proyecto

---
+++ Script.js
@@ -0,0 +1,9 @@
+function openLetter() {
+    const letter = document.getElementById("letter");
+    letter.classList.add("show");
+}
+
+function closeLetter() {
+    const letter = document.getElementById("letter");
+    letter.classList.remove("show");
+}
